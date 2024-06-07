762b0ae2a5a2b4588409d430f8f685485cada4ef
Marcus Lim <markerlim15@gmail.com>
2024-06-07 10:43:23 +0800

    added ionic capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'GeekstackV1.0',
+  appName: 'Geekstack',
+  webDir: 'build'
+};
+
+export default config;
